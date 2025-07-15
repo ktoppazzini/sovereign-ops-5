d8e594d9f30e337800807592d95f22b422d6af32
ktoppazzini <ktoppazzini@tlleanmanagement.com>
2025-07-14 21:49:46 -0400

    Add files via upload
    Uploading app structure for Vercel deployment

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: 'Sovereign Ops',
+  description: 'Empire Reform Generator'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
