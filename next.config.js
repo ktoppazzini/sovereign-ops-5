d33ae87afa2fce128cf6aaa88a61e3de2914dc79
ktoppazzini <ktoppazzini@tlleanmanagement.com>
2025-07-20 12:54:03 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  experimental: {
-    appDir: true
-  }
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+}
+
+module.exports = nextConfig
