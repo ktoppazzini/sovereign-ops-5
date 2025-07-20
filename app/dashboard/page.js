a9a8eefa7449f174fe92cea62c84396fe81bf473
ktoppazzini <ktoppazzini@tlleanmanagement.com>
2025-07-20 17:55:57 -0400

    Create page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welcome to Your Dashboard</h1>
+      <p>You are now securely logged in. 🎉</p>
+    </div>
+  );
+}
