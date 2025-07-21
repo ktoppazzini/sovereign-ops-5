9dfe406aff04c21576ee1c248d669a94c89ad71c
ktoppazzini <ktoppazzini@tlleanmanagement.com>
2025-07-21 14:26:18 -0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,20 @@
 'use client';
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function RedirectHome() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/login');
+  }, [router]);
+
+  return null;
+}
+
+
+
 import { useState } from 'react';
 
 export default function LoginPage() {
