d246b46ec1ded6975219267866c98bc3c9fc3345
ktoppazzini <ktoppazzini@tlleanmanagement.com>
2025-07-21 21:26:52 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,13 @@
+import './globals.css';
 
 export const metadata = {
   title: 'Sovereign Ops',
-  description: 'Empire Reform Generator'
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
